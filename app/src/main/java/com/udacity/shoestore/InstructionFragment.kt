2e16b282ecc4e59c2737baf79e187c07b77a2d60
Vankó Ádám <vankoadam85@gmail.com>
2021-02-01 21:46:04 +0100

    add InstructionFragment.kt and layout

---
+++ app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
@@ -0,0 +1,25 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentInstructionBinding
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class InstructionFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+
+        return binding.root
+    }
+}
