608262418521202ae354d9ecd2a077eb7d9a1158
Vankó Ádám <vankoadam85@gmail.com>
2021-02-01 21:24:32 +0100

    add WelcomeFragment.kt and layout

---
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -0,0 +1,26 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+
+        return binding.root
+    }
+}
