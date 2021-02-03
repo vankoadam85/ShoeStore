e7da13125c09019c05f62d398ea09f5ce60ab2cf
Vankó Ádám <vankoadam85@gmail.com>
2021-02-03 22:31:17 +0100

    refactor ShoeDetailFragment.kt with data binding

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.InverseMethod
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
@@ -15,16 +16,20 @@ class ShoeDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeDetailBinding
     private val activityViewModel by activityViewModels<MainViewModel>()
+    private val shoe = Shoe("", 0.0, "", "")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_shoe_detail, container, false)
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_shoe_detail, container, false)
+
+        binding.shoe = shoe
 
         binding.saveButton.setOnClickListener {
-            activityViewModel.addShoe(createShoe())
+            activityViewModel.addShoe(shoe)
             findNavController().popBackStack()
         }
         binding.cancelButton.setOnClickListener {
@@ -33,13 +38,16 @@ class ShoeDetailFragment : Fragment() {
 
         return binding.root
     }
+}
 
-    private fun createShoe(): Shoe {
-        return Shoe(
-            binding.shoeNameEditText.text.toString(),
-            binding.shoeSizeEditText.text.toString().toDoubleOrNull() ?: 0.0,
-            binding.shoeCompanyEditText.text.toString(),
-            binding.shoeDescriptionEditText.text.toString()
-        )
+object ShoeSizeConverter {
+
+    @InverseMethod("stringToDouble")
+    @JvmStatic fun doubleToString(value: Double): String {
+        return if (value > 0.0) value.toString() else ""
+    }
+
+    @JvmStatic fun stringToDouble(value: String): Double {
+        return value.toDoubleOrNull() ?: 0.0
     }
 }
